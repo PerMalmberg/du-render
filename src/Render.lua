94001eef3cfb0472f0c1884c30080e704c8ed975
Per Malmberg <per.malmberg@gmail.com>
2022-10-28 07:52:42 +0200

    Expand aliases

--- src/Render.lua
+++ src/Render.lua
@@ -27,63 +27,60 @@ AlignVer = {
     Descender = 5,
 }
 
----@alias n number
----@alias i integer
-
 ---@class Render
----@field addBezier fun(layer:i, x1:n, y1:n, x2:n, y2:n, x3:n, y3:n)
----@field addBox fun(layer:i, x:n, y:n, width:n, height:n)
----@field addBoxRounded fun(layer:i, x:n, y:n, width:n, height:n, radius:n)
----@field addCircle fun(layer:i, x:n, y:n, radius:n)
----@field addImage fun(layer:i, image:i, x:n, y:n, width:n, height:n)
----@field addImageSub fun(layer:i, image:i, x:n, y:n, width:n, height:n, subX:n, subY:n, subWidth:n, subHeight:n)
----@field addLine fun(layer:i, x1:n, y1:n, x2:n, y2:n)
----@field addQuad fun(layer:i, x1:n, y1:n, x2:n, y2:n, x3:n, y3:n, x4:n, y4:n)
----@field addText fun(layer:i, font:i, text:string, x:n, y:n)
----@field addTriangle fun(layer:i, x1:n, y1:n, x2:n, y2:n, x3:n, y3:n)
----@field createLayer fun():i
----@field getAvailableFontCount fun():i
+---@field addBezier fun(layer:integer, x1:number, y1:number, x2:number, y2:number, x3:number, y3:number)
+---@field addBox fun(layer:integer, x:number, y:number, width:number, height:number)
+---@field addBoxRounded fun(layer:integer, x:number, y:number, width:number, height:number, radius:number)
+---@field addCircle fun(layer:integer, x:number, y:number, radius:number)
+---@field addImage fun(layer:integer, image:integer, x:number, y:number, width:number, height:number)
+---@field addImageSub fun(layer:integer, image:integer, x:number, y:number, width:number, height:number, subX:number, subY:number, subWidth:number, subHeight:number)
+---@field addLine fun(layer:integer, x1:number, y1:number, x2:number, y2:number)
+---@field addQuad fun(layer:integer, x1:number, y1:number, x2:number, y2:number, x3:number, y3:number, x4:number, y4:number)
+---@field addText fun(layer:integer, font:integer, text:string, x:number, y:number)
+---@field addTriangle fun(layer:integer, x1:number, y1:number, x2:number, y2:number, x3:number, y3:number)
+---@field createLayer fun():integer
+---@field getAvailableFontCount fun():integer
 ---@field getAvailableFontName fun(index):string
----@field getCursor fun():n,n Returns a tuple containing the (x, y) coordinates of the cursor, or (-1, -1) if the screen is not currently raycasted
+---@field getCursor fun():number,number Returns a tuple containing the (x, y) coordinates of the cursor, or (-1, -1) if the screen is not currently raycasted
 ---@field getCursorDown fun():boolean True if the mouse cursor is currently pressed down on the screen, false otherwise
 ---@field getCursorPressed fun():boolean True if the mouse cursor has been pressed down on the screen at any time since the last script execution, false otherwise
 ---@field getCursorReleased fun():boolean True if the mouse cursor has been released on the screen at any time since the last script execution, false otherwise
----@field getDeltaTime fun():n Return the time, in seconds, since the screen was last updated.
----@field getFontMetrics fun(font:i):n,n A tuple containing the maximal ascender and descender, respectively, of the given font
----@field getFontSize fun(font:i):n The font size in vertical pixels
----@field getImageSize fun(image:i):n,n A tuple containing the width and height, respectively, of the image, or (0, 0) if the image is not yet loaded
+---@field getDeltaTime fun():number Return the time, in seconds, since the screen was last updated.
+---@field getFontMetrics fun(font:integer):number,number A tuple containing the maximal ascender and descender, respectively, of the given font
+---@field getFontSize fun(font:integer):number The font size in vertical pixels
+---@field getImageSize fun(image:integer):number,number A tuple containing the width and height, respectively, of the image, or (0, 0) if the image is not yet loaded
 ---@field getInput fun():string The input string, as set by the screen unit API function setScriptInput, or an empty string if there is no current input
 ---@field getLocale fun():string The locale, currently one of "en-US", "fr-FR", or "de-DE"
----@field getRenderCost fun():n The cost of all rendering operations performed by the render script so far (at the time of the call to this function)
----@field getRenderCostMax fun():n The render cost limit. A script that exceeds this limit (in one execution) will not render correctly and will instead throw an error. Note that this value may change between version releases
----@field getResolution fun():n,n A tuple containing the (width, height) of the screen's render surface, in pixels
----@field getTextBounds fun(font:i, text:string):n,n A tuple containing the width and height, respectively, of the bounding box
----@field getTime fun():n Time, in seconds, since the render script started running
----@field isImageLoaded fun(image:i):boolean True if the image is fully loaded and ready to use, false otherwise
----@field loadImage fun(url:string):i Load an image to be used with addImage from the given URL
----@field loadFont fun(name:string, size:i):i Load a font to be used with addText
+---@field getRenderCost fun():number The cost of all rendering operations performed by the render script so far (at the time of the call to this function)
+---@field getRenderCostMax fun():number The render cost limit. A script that exceeds this limit (in one execution) will not render correctly and will instead throw an error. Note that this value may change between version releases
+---@field getResolution fun():number,number A tuple containing the (width, height) of the screen's render surface, in pixels
+---@field getTextBounds fun(font:integer, text:string):number,number A tuple containing the width and height, respectively, of the bounding box
+---@field getTime fun():number Time, in seconds, since the render script started running
+---@field isImageLoaded fun(image:integer):boolean True if the image is fully loaded and ready to use, false otherwise
+---@field loadImage fun(url:string):integer Load an image to be used with addImage from the given URL
+---@field loadFont fun(name:string, size:integer):integer Load a font to be used with addText
 ---@field logMessage fun(message:string) Log a message for debugging purposes. If the "enable output in Lua channel" box is checked on the editor panel for the given screen, the message will be displayed in the Lua channel.
----@field requestAnimationFrame fun(frames:i) Request that this screen should be redrawn in a certain number of frames. A screen that requires highly-fluid animations should call requestAnimationFrame(1) before it returns.
----@field setBackgroundColor fun(red:n, green:n, blue:n) Set the background color of the screen
----@field setDefaultFillColor fun(layer:i, shape:ShapeType, red:n, green:n, blue:n, alpha:n)
----@field setDefaultRotation fun(layer:i, shape:ShapeType, rotation:number)
----@field setDefaultShadow fun(layer:i, shape:ShapeType, radius:n, red:n, green:n, blue:n, alpha:n) Set the default shadow for all subsequent shapes of the given type added to the given layer
----@field setDefaultStrokeColor fun(layer:i, shape:ShapeType, red:n, green:n, blue:n, alpha:n)
----@field setDefaultStrokeWidth fun(layer:i, shape:ShapeType, strokeWidth:n)
----@field setDefaultTextAlign fun(layer:i, hor:AlignHor, ver:AlignVer)
----@field setFontSize fun(font:i, size:i) Set the size at which a font will render.
----@field setLayerClipRect fun(layer:i, x:n, y:n, width:n, height:n) Set a clipping rectangle applied to the layer as a whole.
----@field setLayerOrigin fun(layer:i, x:n, y:n) Set the transform origin of a layer; layer scaling and rotation are applied relative to this origin
----@field setLayerRotation fun(layer:i, rotationRad:n) Set a rotation applied to the layer as a whole, relative to the layer's transform origin
----@field setLayerScale fun(layer:i, widthScale:n, hightScale:n) Set a scale factor applied to the layer as a whole, relative to the layer's transform origin.
----@field setLayerTranslation fun(layer:i, tx:n, ty:n) Set a translation applied to the layer as a whole
----@field setNextFillColor fun(layer:i, red:n, green:n, blue:n, alpha:n) Set the fill color of the next rendered shape on the given layer; has no effect on shapes that do not support a fill color
----@field setNextRotation fun(layer:i, rotationRad:n) Set the rotation of the next rendered shape on the given layer; has no effect on shapes that do not support rotation
----@field setNextRotationDegrees fun(layer:i, rotationDeg:n) Set the rotation of the next rendered shape on the given layer; has no effect on shapes that do not support rotation
----@field setNextShadow fun(layer:i, radius, red:n, green:n, blue:n, alpha:n) Set the shadow of the next rendered shape on the given layer; has no effect on shapes that do not support a shadow
----@field setNextStrokeColor fun(layer:i, red:n, green:n, blue:n, alpha:n) Set the stroke color of the next rendered shape on the given layer; has no effect on shapes that do not support a stroke color
----@field setNextStrokeWidth fun(layer:i, strokeWidth:i) Set the stroke width of the next rendered shape on the given layer; has no effect on shapes that do not support a stroke width
----@field setNextTextAlign fun(layer:i, hor:AlignHor, ver:AlignVer) Set the text alignment of the next rendered text string on the given layer. By default, text is anchored horizontally on the left, and vertically on the baseline.
+---@field requestAnimationFrame fun(frames:integer) Request that this screen should be redrawn in a certain number of frames. A screen that requires highly-fluid animations should call requestAnimationFrame(1) before it returns.
+---@field setBackgroundColor fun(red:number, green:number, blue:number) Set the background color of the screen
+---@field setDefaultFillColor fun(layer:integer, shape:ShapeType, red:number, green:number, blue:number, alpha:number)
+---@field setDefaultRotation fun(layer:integer, shape:ShapeType, rotation:number)
+---@field setDefaultShadow fun(layer:integer, shape:ShapeType, radius:number, red:number, green:number, blue:number, alpha:number) Set the default shadow for all subsequent shapes of the given type added to the given layer
+---@field setDefaultStrokeColor fun(layer:integer, shape:ShapeType, red:number, green:number, blue:number, alpha:number)
+---@field setDefaultStrokeWidth fun(layer:integer, shape:ShapeType, strokeWidth:number)
+---@field setDefaultTextAlign fun(layer:integer, hor:AlignHor, ver:AlignVer)
+---@field setFontSize fun(font:integer, size:integer) Set the size at which a font will render.
+---@field setLayerClipRect fun(layer:integer, x:number, y:number, width:number, height:number) Set a clipping rectangle applied to the layer as a whole.
+---@field setLayerOrigin fun(layer:integer, x:number, y:number) Set the transform origin of a layer; layer scaling and rotation are applied relative to this origin
+---@field setLayerRotation fun(layer:integer, rotationRad:number) Set a rotation applied to the layer as a whole, relative to the layer's transform origin
+---@field setLayerScale fun(layer:integer, widthScale:number, hightScale:number) Set a scale factor applied to the layer as a whole, relative to the layer's transform origin.
+---@field setLayerTranslation fun(layer:integer, tx:number, ty:number) Set a translation applied to the layer as a whole
+---@field setNextFillColor fun(layer:integer, red:number, green:number, blue:number, alpha:number) Set the fill color of the next rendered shape on the given layer; has no effect on shapes that do not support a fill color
+---@field setNextRotation fun(layer:integer, rotationRad:number) Set the rotation of the next rendered shape on the given layer; has no effect on shapes that do not support rotation
+---@field setNextRotationDegrees fun(layer:integer, rotationDeg:number) Set the rotation of the next rendered shape on the given layer; has no effect on shapes that do not support rotation
+---@field setNextShadow fun(layer:integer, radius, red:number, green:number, blue:number, alpha:number) Set the shadow of the next rendered shape on the given layer; has no effect on shapes that do not support a shadow
+---@field setNextStrokeColor fun(layer:integer, red:number, green:number, blue:number, alpha:number) Set the stroke color of the next rendered shape on the given layer; has no effect on shapes that do not support a stroke color
+---@field setNextStrokeWidth fun(layer:integer, strokeWidth:integer) Set the stroke width of the next rendered shape on the given layer; has no effect on shapes that do not support a stroke width
+---@field setNextTextAlign fun(layer:integer, hor:AlignHor, ver:AlignVer) Set the text alignment of the next rendered text string on the given layer. By default, text is anchored horizontally on the left, and vertically on the baseline.
 ---@field setOutput fun(output:string) Set the script's output string, which can be retrieved via a programming board with the screen unit API function getScriptOutput
 
 
