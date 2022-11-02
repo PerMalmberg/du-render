3089a509a55484223f8e76195aca663445bb0d1a
Per Malmberg <per.malmberg@gmail.com>
2022-11-02 22:56:36 +0100

    New default for text align.

--- src/TextAlign.lua
+++ src/TextAlign.lua
@@ -1,6 +1,7 @@
 ---@class TextAlign
 ---@field Hor RSAlignHor
 ---@field Ver RSAlignVer
+---@field Default fun():TextAlign
 
 local TextAlign = {}
 TextAlign.__index = TextAlign
@@ -17,8 +18,10 @@ function TextAlign.New(horizontal, vertical)
     return setmetatable(s, TextAlign)
 end
 
+---Creates a default text alignment
+---@return TextAlign
 function TextAlign.Default()
-    return TextAlign.New(RSAlignHor.Left, RSAlignVer.Bottom)
+    return TextAlign.New(RSAlignHor.Left, RSAlignVer.Top)
 end
 
 return TextAlign
